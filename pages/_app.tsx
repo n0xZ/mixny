b10b8d3c47ca0f7b938617685b43b10433674401
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-04-18 15:47:31 -0300

    set chakraprovider and custom fonts from fontsource

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import '@fontsource/roboto/300.css'
+import '@fontsource/lato/300.css'
+import '@fontsource/montserrat/300.css'
 
+import {theme} from '../theme';
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<ChakraProvider theme={theme}>
+			<Component {...pageProps} />
+		</ChakraProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
