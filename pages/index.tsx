b4532de0940823c8a88a64dceffd8ea475d221ad
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-04-19 17:14:34 -0300

    set sidebar component on Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,21 +7,34 @@ import Head from 'next/head';
 import { useState } from 'react';
 import Layout from '@/components/Layout';
 import PositionList from '@/components/Positions/PositionList';
-import { PositionResult, Position } from '../types';
 import {
 	Container,
-	Divider,
 	Flex,
 	Stack,
 	Text,
 } from '@chakra-ui/react';
+import Sidebar from '@/components/Sidebar';
+import { PositionResult, Position } from '../types';
 const Home: NextPage = ({
 	positions,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
 	const [actualPositions, setActualPositions] =
 		useState<Position[]>(positions);
 	const [search, setSearch] = useState<string>('');
-
+	const handleInputChange = (
+		e: React.ChangeEvent<HTMLInputElement>
+	) => {
+		const { value } = e.target;
+		setSearch(value);
+		const filteredPositions = positions.filter(
+			(position: Position) => {
+				return position.title
+					.toLowerCase()
+					.includes(value.toLowerCase());
+			}
+		);
+		setActualPositions(filteredPositions);
+	};
 	return (
 		<Layout>
 			<Head>
@@ -32,15 +45,22 @@ const Home: NextPage = ({
 				/>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
+			<Flex w="full" h="full" as="section">
+				<Sidebar
+					inputValue={search}
+					handleInputChange={handleInputChange}
+					categories={0}
+				/>
 
-			<Container maxW="container.xl" as="section">
-				<Text as="span" fontSize="lg" fontWeight="bold">
-					Open positions ({positions.length})
-				</Text>
-				<Stack flex={1} flexDir="column" alignItems="center">
-					<PositionList positions={positions} />
-				</Stack>
-			</Container>
+				<Container maxW="container.xl" h="full" minH="100vh">
+					<Text as="span" fontSize="lg" fontWeight="bold">
+						Open positions ({positions.length})
+					</Text>
+					<Stack flex={1} flexDir="column" alignItems="center">
+						<PositionList positions={positions} />
+					</Stack>
+				</Container>
+			</Flex>
 		</Layout>
 	);
 };
