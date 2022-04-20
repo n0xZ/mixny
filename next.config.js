71b5bda971e69ebb39173a1da8a67d8f8fb16472
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-04-20 16:57:24 -0300

    set i18 locales

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  env:{
-    API_URL: process.env.MAJORKEYS_API_URL,
-  }
-}
+	reactStrictMode: true,
+	env: {
+		API_URL: process.env.MAJORKEYS_API_URL,
+	},
+	
+	i18n: {
+		locales: ['es', 'en'],
+		defaultLocale: 'es',
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
